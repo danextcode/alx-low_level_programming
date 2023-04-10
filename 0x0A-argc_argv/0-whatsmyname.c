825593ffd5cd0b44f8ae68a0de5308070baa543c
Yebung Solomon Danladi <ysmaxdan@gmail.com>
2023-04-10 03:32:49 -0700

    Task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+/*
+ * File: 0-whatsmyname.c
+ * Author: Yebung Danladi
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
+
+int main(int _attribute_((_unused_)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
