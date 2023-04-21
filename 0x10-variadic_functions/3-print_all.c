58d0fd8e51818fb93f66adc1cbb78dc7a51a7906
Yebung Solomon Danladi <ysmaxdan@gmail.com>
2023-04-21 13:15:20 -0700

    Corrected task 4

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,47 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
-/**
- * print_all - prints anything.
- * @format: a list of types of arguments passed to the function.
- * Return: no return.
- */
+void op_char(char)
+{
+	printf("%c", va_arg(pseudo, char));
+}
+
+void po_int(int)
+{
+	printf("%d", va_arg(pseudo, int));
+}
+
+void op_float(float)
+{
+	printf("%f", va_arg(pseudo, float));
+}
+
+void op_string(char *)
+{
+	printf("%s", va_arg(pseudo, char *));
+}
+
 void print_all(const char * const format, ...)
 {
-	va_list valist;
-	unsigned int i = 0, j, c = 0;
-	char *str;
-	const char t_arg[] = "cifs";
+	va_list pseudo;
+	va_start(pseudo, format);
 
-	va_start(valist, format);
-	while (format && format[i])
-	{
-		j = 0;
-		while (t_arg[j])
-		{
-			if (format[i] == t_arg[j] && c)
-			{
-				printf(", ");
-				break;
-			} j++;
-		}
-		switch (format[i])
-		{
-		case 'c':
-			printf("%c", va_arg(valist, int)), c = 1;
-			break;
-		case 'i':
-			printf("%d", va_arg(valist, int)), c = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(valist, double)), c = 1;
-			break;
-		case 's':
-			str = va_arg(valist, char *), c = 1;
-			if (!str)
-			{
-				printf("(nil)");
-				break;
-			}
-			printf("%s", str);
-			break;
-		} i++;
-	}
-	printf("\n"), va_end(valist);
+	op_t ops[] = {
+		{"c", op_char},
+		{"i", op_int},
+		{"f", op_float},
+		{"s", op_string},
+		{NULL, NULL}
+	};
+	int i;
+
+
+	while()
+	while()
+		if()
+		if()
+		if()
+
+	printf("\n");
 }
